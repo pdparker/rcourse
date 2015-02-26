c07ecc72d26d00f39443752812dc2dd4d1dd5216
Philip Parker <philip.parker@acu.edu.au>
2015-02-26 14:57:53 +1100

    Added multilevel components

--- templateFiles/regression.R
+++ templateFiles/regression.R
@@ -44,3 +44,74 @@ modelSesAch <- lm(SCMAT ~ ESCS + PV1MATH + PV1READ + PV1SCIE, data = pisa)
 print(summary(modelSesAch))
 # Compare the models 
 print(anova(modelSes, modelSesAch))
+
+# first I am going to z-score all achievement scores
+pisa[, grep("^PV[1-5].*", names(pisa))] <- apply(pisa[, grep("^PV[1-5].*", names(pisa))],
+												 2, scale)
+#model without moderation
+modelMain <- lm(SCMAT ~ PV1MATH + gender, data = pisa)
+print(summary(modelMain))
+#Model with interaction
+modelInt <- lm(SCMAT ~ PV1MATH*gender, data = pisa)
+print(summary(modelInt))
+#Compute simple effect
+library(car)
+deltaMethod(modelInt,g = "ach + int*1",vcov. = vcov(modelInt),parameterNames = c("int", "ach", "gen", "int"))
+deltaMethod(modelInt,g = "ach + int*2",vcov. = vcov(modelInt),parameterNames = c("int", "ach", "gen", "int"))
+
+
+#load the mediation package
+library(mediation)
+# here we will use the subset command to account for missing data on SCMAT
+# First we explore the effect of gender on achievement
+modelA <- lm(PV1MATH ~ gender, data = pisa)
+# Second we estimate the effect of gender and PV1MATH allowing for a treatment by mediator interaction
+modelB <- lm(SCMAT ~ gender*PV1MATH, data = pisa, subset = !is.na(SCMAT) )
+# Now we calculate the mediation using simulation (see help file for how to do bootstrapping)
+modelAB <- mediate(modelA, modelB, treat = 'gender', mediator = 'PV1MATH', sims = 100)
+# Get the results and plot them
+print(summary(modelAB) )
+plot(modelAB)
+
+#First lets create a blue colar versus white colar variable
+# We will do this via the recode function in the car package
+library(car)
+pisa$socialClass <- recode(pisa$SSECATEG, "c(1,2) = 0; c(3,4) = 1")
+# now we can run our logistic regression
+modelLogistic <- glm(socialClass ~ ESCS, data = pisa, family = binomial(link = 'logit') )
+# Summary of the model
+print(summary(modelLogistic) )
+# Coefs in log odds
+print(exp(coef(modelLogistic) ))
+
+modelRandomSlope <- lmer(SCMAT ~ PV1MATH + (PV1MATH|SCHOOLID.x) , data = pisa)
+
+
+#plot
+visualize.lme <- function (model, coefficient, group, ...){
+	r <- ranef(model)[[group]]
+	f <- fixef(model)
+	
+	effects <- data.frame(r[,1]+f[1], r[,2]+f[2])
+	
+	number.lines <- nrow(effects)
+	
+	predictor.min <- tapply(model$data[[coefficient]], model$data[[group]], min, na.rm=TRUE)
+	predictor.max <- tapply(model$data[[coefficient]], model$data[[group]], max, na.rm=TRUE)
+	
+	outcome.min <- min(predict(model), na.rm=TRUE)
+	outcome.max <- max(predict(model), na.rm=TRUE)
+	
+	plot (c(min(predictor.min),max(predictor.max)),c(outcome.min,outcome.max), type="n", ...)
+	
+	for (i in 1:number.lines){
+		expression <- function(x) {effects[i,1] + (effects[i,2] * x) }
+		curve(expression, from=predictor.min[i], to=predictor.max[i], add=TRUE)
+	}
+}
+
+
+
+visualize.lme(modelRandomSlope, "standLRT", "SCHOOLID.x", 
+			  xlab="Achievement in Math", ylab="self-concept",
+			  main="Effect of achievement on self-concept stratified by school")
